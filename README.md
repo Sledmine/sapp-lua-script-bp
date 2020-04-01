dac7f440071ea818fc5e17c0d3db533457266e79
Sledmine <angelherxd@hotmail.com>
2020-04-01 09:18:43 -0700

    updated README.md, boilerplate script done

--- README.md
+++ README.md
@@ -26,3 +26,16 @@ And run them adding this line in your **init.txt** file, one line per file:
 ```
 lua_load <example_script_name>
 ```
+
+## Adding libraries to your server
+
+You will find some of the "must-have" libraries for your scripting projects that are going to help you with fast developing and debugging.
+
+- LuaBlam [Github](https://github.com/Sledmine/lua-blam)
+- JSON [Github](https://github.com/rxi/json.lua)
+- Inspect [Github](https://github.com/kikito/inspect.lua)
+
+You can download them from their respective repositiories and place them into your root game/server folder in a specific directory called **\lua**:
+```
+"Halo Custom Edition\lua"
+```
--- boilerplate_script.lua
+++ boilerplate_script.lua
@@ -1,13 +1,33 @@
 -- SAPP Lua Script Boilerplate
 -- Version 1.0
+-- Every function uses lower camel case, be careful about naming conventions
 
-api_version = "1.12.0.0"
+-- Api version must be declared at the top
+-- It helps lua-blam to detect if the script is made for SAPP or Chimera
+api_version = '1.12.0.0'
+
+-- Lua libraries
+local inspect = require 'inspect'
+local json = require 'json'
+
+-- Halo Custom Edition specific libraries
+local blam = require 'lua-blam'
+
+-- On tick function provided by default if needed
+-- Be careful at handling data here, things can be messy
+function onTick()
+end
+
+-- Put initialization code here
 function OnScriptLoad()
- -- Put initialization code here.
+    -- We can set up our event callbacks, like the onTick callback
+    --register_callback(cb['EVENT_TICK'], "onTick")
 end
+
+-- Put cleanup code here
 function OnScriptUnload()
- -- Put cleanup code here.
 end
+
+-- This function is not mandatory, but if you want to log errors, use this
 function OnError(Message)
- -- This function is not required, but if you want to log errors, use this.
 end
